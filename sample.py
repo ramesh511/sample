7a20ce08dff1dcd4b5d8223636a95d6cf740ae5a
ramesh <ramesh.manickam@microchip.com>
2017-04-28 10:15:38 +0530

    Printing of argument is commented.

--- sample.py
+++ sample.py
@@ -12,18 +12,19 @@ def read_dos(eval_path):
 
     
 
-def main():
+def main_function_call():
 
     samp_parser = argparse.ArgumentParser()
 
-    samp_parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2],
+    samp_parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2],dest="verbose_inp",
                         help="increase output verbosity")
 
-    samp_parser.add_argument("-x", "--xml", type=str,
+    samp_parser.add_argument("-x", type=str,dest="xml",
                              help="path to XML file to process", required=True)
-   
     samp_args = samp_parser.parse_args()
 
+   # print "ARGUMENT {0}".format(samp_args)
+	
     check_file = samp_args.xml
     
     if check_file:
@@ -33,4 +34,4 @@ def main():
 
         
 if __name__ == "__main__":
-    main()
+    main_function_call()
